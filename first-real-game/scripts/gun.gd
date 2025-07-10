3a8e010efceceece884cb74cb6f17c4bc49940a7
Erik Hostnik <erik.hostnik@gmail.com>
2025-07-10 15:14:37 +0200

    Added shooting/fire mechanic

--- first-real-game/scripts/gun.gd
+++ first-real-game/scripts/gun.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const BULLET = preload("res://scenes/bullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,4 +16,11 @@ func _process(delta: float) -> void:
 		scale.y = -1
 	else:
 		scale.y = 1
+		
+		
+	if Input.is_action_just_pressed("fire"):
+		var bullet_instance = BULLET.instantiate()   
+		get_tree().root.add_child(bullet_instance)         #added to the game 
+		bullet_instance.global_position = global_position  #the same rotation as the gun and position
+		bullet_instance.rotation = rotation
 		
