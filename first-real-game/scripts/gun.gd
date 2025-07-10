19b8f5e78e8538db23b08772dfbf76d0e72c82c3
Erik Hostnik <erik.hostnik@gmail.com>
2025-07-10 15:36:33 +0200

    Added enemy spprites

--- first-real-game/scripts/gun.gd
+++ first-real-game/scripts/gun.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const BULLET = preload("res://scenes/bullet.tscn")
+@onready var muzzle: Marker2D = $Muzzle
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,6 +22,6 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("fire"):
 		var bullet_instance = BULLET.instantiate()   
 		get_tree().root.add_child(bullet_instance)         #added to the game 
-		bullet_instance.global_position = global_position  #the same rotation as the gun and position
+		bullet_instance.global_position = muzzle.global_position  #the same rotation as the gun and position
 		bullet_instance.rotation = rotation
 		
